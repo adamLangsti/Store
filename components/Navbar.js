a3b911a13c496a9a0d676edd00512e06ce5d77da
Adam Lang <adam.lang-1@hotmail.com>
2023-01-07 15:52:40 +0200

    feat: created function that makes fetch request for the items in our cart

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,6 +10,24 @@ const NavbarComponent = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const checkout = async () => {
+        await fetch('http://localhost:4000/checkout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ items: cart.items }),
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                if (response.url) {
+                    window.location.assign(response.url);
+                }
+            });
+    };
+
     const productsCount = cart.items.reduce(
         (sum, product) => sum + product.quantity,
         0
@@ -43,7 +61,9 @@ const NavbarComponent = () => {
                             ))}
                             <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
 
-                            <Button variant='success'>Purchase items!</Button>
+                            <Button variant='success' onClick={checkout}>
+                                Purchase items!
+                            </Button>
                         </>
                     ) : (
                         'There are no items in your cart.'
